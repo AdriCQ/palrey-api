db5813dadd23b27644c89918210673872c83eda9
AdriCQ <adrian.capote95@zoho.com>
2022-06-28 23:11:35 -0400

    Register indexpage

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,10 +2,12 @@
 
 import { createApp } from 'vue';
 import router from './router';
-// import IndexPageVue from './pages/IndexPage.vue';
+import IndexPageVue from './pages/IndexPage.vue';
 import AppVue from './App.vue';
 // Create App
 const app = createApp(AppVue);
+
+app.component('index-page', IndexPageVue);
 
 // Vue Router
 app.use(router);
